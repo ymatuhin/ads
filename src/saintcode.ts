e423406853716eab8f1290ec0001264a11e95666
ymatuhin <ym@ymatuhin.ru>
2022-06-15 23:45:49 +0300

    Check images, add utm for size

--- src/saintcode.ts
+++ src/saintcode.ts
@@ -1,29 +1,29 @@
 const banners = [
-  "nachni_obuchenie_v_saintcode",
-  "prihodi_v_saintcode",
-  "stan_frontend_razrabotchikom",
-  "effektivnye_kursy_po_js",
-  "putaeshsya_v_sintaksise_js",
-  "ne_znaesh_s_chego_nachat",
-  "boishsya_hodit_na_sobes",
-  "hochesh_vo_frontend",
   "bistriy_start_dlya_web_razrabotchika",
-  "ne_online_shkola_a_bootcamp",
-  "kak_ponyat_frontend",
+  "boishsya_hodit_na_sobes",
   "bystro_stat_frontenderom",
-  "online_shkoly_nichemu_ne_nauchili",
+  "effektivnye_kursy_po_js",
+  "hochesh_vo_frontend",
+  "kak_ponyat_frontend",
+  "nachni_obuchenie_v_saintcode",
+  "ne_online_shkola_a_bootcamp",
   "ne_poluchilos_uchitsya_online",
-  "udelyaem_vremya_kazhdomu_studentu",
+  "ne_znaesh_s_chego_nachat",
+  "online_shkoly_nichemu_ne_nauchili",
+  "prihodi_v_saintcode",
+  "putaeshsya_v_sintaksise_js",
   "slozhno_uchitsya_po_vebinaram",
+  "stan_frontend_razrabotchikom",
+  "udelyaem_vremya_kazhdomu_studentu",
 ];
 
 export const getSaintCodeHtml = (block: HTMLElement) => {
   const { clientWidth } = block;
-  const size = clientWidth > 970 ? 970 : clientWidth > 600 ? 768 : 375;
+  const size = clientWidth > 970 ? 970 : clientWidth > 600 ? 728 : 375;
   const bannerName = banners[Math.floor(Math.random() * banners.length)];
-  const baseUrl = "https://ymatuhin.ru/ads";
+  const baseUrl = "https://ymatuhin.ru/ads/saint-code";
   const imageUrl = `${baseUrl}/${bannerName}_${size}.svg`;
-  const linkUrl = `https://saintcode.ru?utm_source=${location.hostname}&utm_medium=banner&utm_campaign=summer_promo_2022&utm_content=${bannerName}&utm_term=ymatuhin`;
+  const linkUrl = `https://saintcode.ru?utm_source=${location.hostname}&utm_medium=banner&utm_campaign=summer_promo_2022&utm_content=${bannerName}&utm_term=ymatuhin&utm_banner_width=${size}`;
 
   // location
   return `SAINT-CODE
